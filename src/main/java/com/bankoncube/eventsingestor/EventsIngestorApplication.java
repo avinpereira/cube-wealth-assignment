056f9a9d38b212795b3f7cd0b8a305b175658bc8
APEREI26 <avin.pereira@decathlon.com>
2020-07-18 13:00:56 +0530

    Initial Commit

---
+++ src/main/java/com/bankoncube/eventsingestor/EventsIngestorApplication.java
@@ -0,0 +1,13 @@
+package com.bankoncube.eventsingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsIngestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsIngestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankoncube/eventsingestor/EventsIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankoncube.eventsingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsIngestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
